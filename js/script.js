29238fb241c0efa4b014357993b8fce2410d7356
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-05-19 23:10:22 +0600

    added all

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+// // demos mixitup-------------
+// $(function(){
+//     $('.mixitup').mixItUp({ 
+//         selectors: {
+//           target: '.tile',
+//           filter: '.filter',
+//           sort: '.sort-btn'
+//         },
+//             animation: {
+//             animateResizeContainer: false,
+//             effects: 'fade scale'
+//         }
+//     });
+// })
